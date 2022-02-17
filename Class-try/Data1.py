57293b8b8ca562885de275fc852a3f940d8d618f
georgesteve <37008877+georgesteve@users.noreply.github.com>
2022-02-17 17:06:25 -0500

    Create Data1.py

---
+++ Class-try/Data1.py
@@ -0,0 +1,8 @@
+class Alumno:
+
+    def __init__(self):
+        self.nombre = "Pablo"
+
+    def saludar(self):
+        """Imprime un saludo en pantalla."""
+        print(f"¡Hola, {self.nombre}!")
