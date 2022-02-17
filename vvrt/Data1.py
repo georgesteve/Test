d51842c4d7e5cca042d1a031bd76cd06dcbf50a2
georgesteve <37008877+georgesteve@users.noreply.github.com>
2022-02-17 17:49:22 -0500

    Create Data1.py

---
+++ vvrt/Data1.py
@@ -0,0 +1,9 @@
+
+class Alumno:
+
+    def __init__(self):
+        self.nombre = "Pablo"
+
+    def saludar(self):
+        """Imprime un saludo en pantalla."""
+        print(f"¡Hola, {self.nombre}!")
